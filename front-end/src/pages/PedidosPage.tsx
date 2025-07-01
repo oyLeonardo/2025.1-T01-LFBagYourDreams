e48adc288b4fecbdb4377d576c02a930c9eaad9b
oyLeonardo <231039113@aluno.unb.br>
2025-07-01 15:50:50 -0300

    feat: tabelas + dadosmockados

--- front-end/src/pages/PedidosPage.tsx
+++ front-end/src/pages/PedidosPage.tsx
@@ -1,9 +1,9 @@
-import Button from "../components/Button"
-import { Link } from "react-router-dom"
+import TabelaPedidos from "./tabelapedidos"
 function PedidosPage(){
 return (
     <>
       <div className="flex flex-row justify-between bg-blue-300 w-full"></div>
+  <TabelaPedidos></TabelaPedidos>
     </>
   )
 }
--- front-end/src/pages/ProdutosPage.tsx
+++ front-end/src/pages/ProdutosPage.tsx
@@ -1,19 +1,17 @@
 import { Link } from "react-router-dom"
 import Button from "../components/Button"
+import TabelaProdutos from "./tabelapedidos"
+
 function ProdutosPage(){
     return(
-        <>
-      <div className="flex flex-row justify-between bg-blue-300 w-full">
-        <h1>PRODUTOS</h1>
-        {/* vamos colocar aqui datas da nossa tabela (intervalos) */}
-        <h2>exemploaaa</h2>
-      </div>
-      <div className="flex flex-row justify-end bg-green-300 w-full">
+    <>
+      <div className="flex flex-row justify-end w-full">
         <Link to="../adicionarproduto">
             <Button name="Adicionar Produto" color="green"></Button>
         </Link>
         {/* <Button name="Excluir"></Button> (TODO)*/}
       </div>
+      <TabelaProdutos></TabelaProdutos>
     </>
     )
 }
